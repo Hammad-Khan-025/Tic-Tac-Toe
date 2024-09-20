8e7aed017100d3b51a941a2b9308cc4a1cb595ee
Hammad Khan <hk188540@gmail.com>
2024-09-20 14:52:07 +0500

    update gameboard

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -5,13 +5,13 @@ const GameBoard = ({board, handleGameBoard}) => {
 
 
   return (
-    <ol className="flex flex-col items-center justify-center gap-4 sm:gap-6 mx-5 mt-10 mb-5">
+    <ol className="flex flex-col items-center justify-center gap-5 sm:gap-6 mx-5 mt-10 mb-5">
       {board.map((row, rowIndex) => (
         <li key={rowIndex}>
-          <ol className="grid grid-cols-3 gap-4 sm:gap-6">
+          <ol className="grid grid-cols-3 gap-5 sm:gap-6">
             {row.map((playerSymbol, colIndex) => (
-              <li key={colIndex} className="h-20">
-                <button className="bg-[#c2b992] w-16 sm:w-20 h-16 sm:h-20 rounded-md text-3xl sm:text-4xl font-extrabold font-serif text-slate-900" 
+              <li key={colIndex} className="h-[68px] sm:h-20">
+                <button className="bg-[#c2b992] w-[68px] sm:w-20 h-[68px] sm:h-20 rounded-md text-3xl sm:text-4xl font-extrabold font-serif text-slate-900" 
                   onClick={()=> handleGameBoard(rowIndex, colIndex)}
                   disabled={!board[rowIndex][colIndex] ? false : true}>
                   {playerSymbol }
