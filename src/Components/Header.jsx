e2e324a1833f798fe8bde651bb7f4c5fc079bd0b
Hammad Khan <hk188540@gmail.com>
2024-09-20 11:33:53 +0500

    fix logo path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '/images/game-logo.png'
 
 const Header = () => {
   return (
     <header className='flex flex-col items-center gap-5 pb-5'>
-      <img src="../images/game-logo.png" alt="Tic Tac Toe Logo" className='w-16'/>
+      <img src={logo} alt="Tic Tac Toe Logo" className='w-16'/>
       <h1 className='text-slate-900 font-serif font-extrabold text-2xl sm:text-3xl'>Tic-Tac-Toe</h1>
     </header>
   )
